5e625cbc24a1b5afa6cdd532db9f829676c5dde2
Anaïs Ramírez <90638339+AnaisRamirez-SagoMini@users.noreply.github.com>
2024-08-02 15:37:07 -0400

    Follow Jinja transform

--- CameraController.cs
+++ CameraController.cs
@@ -3,4 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
+
+	[SerializeField]
+	Transform jinjaTransform = null;
+
+	// Start is called before the first frame update
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+		// get the current position
+		Vector3 current = GetComponent<Transform>().position;
+
+		//get the target position
+		Vector3 target = jinjaTransform.position;
+
+		// make sure the camera's "z" coordinate doesn't change
+		target.z = current.z;
+
+		// get the new position
+		Vector3 position = Vector3.Lerp(current, target, 0.1f);
+
+		//move the camera to the new position
+		GetComponent<Transform>().position = position;
+	}
 }
