9bafbc5e860d51cd40521e7ad08ec9e5450a76cd
Anaïs Ramírez <90638339+AnaisRamirez-SagoMini@users.noreply.github.com>
2024-08-02 13:59:14 -0400

    Moving Jinja to the right

---
+++ JinjaController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class JinjaController : MonoBehaviour {
+	// Start is called before the first frame update
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+		//check if the arrow key is down, move jinja
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			GetComponent<Transform>().position += Vector3.right;
+		}
+
+	}
+}
