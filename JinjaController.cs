92610e496a44d12dec2332335934e9071fbdcccd
Anaïs Ramírez <90638339+AnaisRamirez-SagoMini@users.noreply.github.com>
2024-08-09 14:44:54 -0400

    Update JinjaController.cs

--- JinjaController.cs
+++ JinjaController.cs
@@ -118,5 +118,14 @@ public class JinjaController : MonoBehaviour {
 		}
 
 
+		//if jinja is moving
+		if (Mathf.Abs(velocity.x) >= acceleration) {
+			//get the direction from jinja's velocity, 1 or -1
+			float movementDirection = Mathf.Sign(velocity.x);
+
+			//make her face the right way
+			tf.localScale = new Vector3(movementDirection, 1, 1);
+		}
+
 	}
 }
