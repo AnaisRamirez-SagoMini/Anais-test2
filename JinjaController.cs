a46dd918c292dffdba1466db51e8aa84b88a506b
Anaïs Ramírez <90638339+AnaisRamirez-SagoMini@users.noreply.github.com>
2024-08-18 19:20:38 -0400

    Update JinjaController.cs

--- JinjaController.cs
+++ JinjaController.cs
@@ -7,6 +7,12 @@ public class JinjaController : MonoBehaviour {
 	// a field to store Jinja's velocity
 	Vector3 velocity = Vector3.zero;
 
+	[SerializeField]
+	float acceleration = 0.1f;
+
+	[SerializeField]
+	float friction = 0.9f;
+
 	// Start is called before the first frame update
 	void Start() {
 
@@ -18,16 +24,19 @@ public class JinjaController : MonoBehaviour {
 		//variable to store jinja's transform component
 		Transform tf = GetComponent<Transform>();
 
+		// add friction
+		velocity.x *= friction;
+
 		// check if the right arrow key is down
 		if (Input.GetKey(KeyCode.RightArrow)) {
 			//add to the velocity
-			velocity += Vector3.right * 0.1f;
+			velocity += Vector3.right * acceleration;
 		}
 
 		// check if the left arrow key is down
 		if (Input.GetKey(KeyCode.LeftArrow)) {
 			//reduce velocity
-			velocity += Vector3.left * 0.1f;
+			velocity += Vector3.left * acceleration;
 		}
 
 		//move jinja by her current velocity
